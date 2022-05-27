919f66ac7167f05a5a67bafa13dfe6b42030fa4e
Noriyasu Katano <nori5592@aol.com>
2022-05-27 22:23:32 +0900

    Make func fecth data
    Create function for fetch to data.

--- test_sample/phpRealm/phpRealm/ContentView.swift
+++ test_sample/phpRealm/phpRealm/ContentView.swift
@@ -5,11 +5,11 @@
 //  Created by norikatano on 2022/05/23.
 //
 
-import Foundation
 import SwiftUI
 
 struct ContentView: View {
 
+    @ObservedObject var requestMusicItems = RequestMusicItems()
     @State var models: [MusicItem] = []
 
     var body: some View {
@@ -21,38 +21,8 @@ struct ContentView: View {
                     Text(model.author ?? "").font(.callout)
                 }
             }
-
         }.onAppear(perform: {
-            guard let url: URL = URL(string: "http://reading-sound.ciao.jp/nori_document/get_music.php") else {
-                print("invalid URL")
-                return
-            }
-
-            var urlRequest: URLRequest = URLRequest(url: url)
-            urlRequest.httpMethod = "GET"
-            URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
-                guard let data = data else {
-                    print("invalid response")
-                    return
-                }
-                do {
-                    let decoder: JSONDecoder = JSONDecoder()
-                    self.models = try decoder.decode([MusicItem].self, from: data)
-                } catch {
-                    print(String(describing: error))
-                    print(error.localizedDescription)
-                }
-            }).resume()
+            self.models = requestMusicItems.models
         })
     }
-}
-
-class MusicItem: Codable, Identifiable {
-    var id: String? = ""
-    var title: String? = ""
-    var author: String? = ""
-    var description: String? = ""
-    var url: String? = ""
-    var time: String? = ""
-    var version: String? = ""
 }
